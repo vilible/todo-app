50138c8d3af9273f710320527b8a6fe4debdec36
vilible <stefanskipiotr.8@proton.me>
2023-03-17 11:05:34 +0100

    Declaring all necessary components and linking some of them to HTML templates

--- src/website/views.py
+++ src/website/views.py
@@ -13,18 +13,35 @@ def index():
 @views.route("/add", methods=["POST"])
 def add():
     description = request.form.get("task")
-    is_done = False
-    date = datetime.now().strftime("%d.%m.%Y %H:%M")
 
-    db.session.add(Tasks(description=description, is_done=is_done, date=date))
-    db.session.commit()
+    if description != "":
+        is_done = False
+        date = datetime.now().strftime("%d.%m.%Y %H:%M")
+
+        db.session.add(Tasks(description=description, is_done=is_done, date=date))
+        db.session.commit()
 
     return redirect(url_for("views.index"))
 
 @views.route("/edit/<int:id>", methods=["GET", "POST"])
 def edit(id: int):
-    pass
+    task = Tasks.query.filter_by(id=id).first()
+
+    if task == None:
+        return redirect(url_for("views.index"))
+
+    if request.method == "POST":
+        task.description = request.form.get("task")
+        task.is_done = True if request.form.get("is_done") == "on" else False
+        db.session.commit()
+
+        return redirect(url_for("views.index"))
+    else:
+        return render_template("edit.html", id=id, description=task.description, is_done=task.is_done)
 
 @views.route("/delete/<int:id>")
 def delete(id: int):
-    pass
+    db.session.delete(Tasks.query.filter_by(id=id).first())
+    db.session.commit()
+
+    return redirect(url_for("views.index"))
