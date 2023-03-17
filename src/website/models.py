62412220d2f7df5a42c4a1f8df492744f150afa6
vilible <stefanskipiotr.8@proton.me>
2023-03-17 11:04:15 +0100

    Changing model's attributes

--- src/website/models.py
+++ src/website/models.py
@@ -2,6 +2,6 @@ from . import db
 
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(200))
+    description = db.Column(db.Text)
     is_done = db.Column(db.Boolean)
     date = db.Column(db.String(20))
