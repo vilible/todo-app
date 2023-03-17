e4f73ae044e032b435e1e572c1ddfc81ed28114a
vilible <stefanskipiotr.8@proton.me>
2023-03-17 07:43:54 +0100

    Linking views to the website

---
+++ src/website/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+from .views import views
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "dev"
+
+    app.register_blueprint(views, url_prefix="/")
+
+    return app
