f201491ba009f4d527eb8bcb9bdd3ec69626d13e
vilible <stefanskipiotr.8@proton.me>
2023-03-17 10:03:27 +0100

    Changing Tasks model for new type of information

--- src/website/models.py
+++ src/website/models.py
@@ -4,4 +4,4 @@ class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200))
     is_done = db.Column(db.Boolean)
-    date = db.Column(db.DateTime(timezone=True))
+    date = db.Column(db.String(20))
--- src/website/views.py
+++ src/website/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from datetime import datetime
+
 from .models import Tasks
+from . import db
 
 views = Blueprint("views", __name__)
 
@@ -9,7 +12,14 @@ def index():
 
 @views.route("/add", methods=["POST"])
 def add():
-    pass
+    description = request.form.get("task")
+    is_done = False
+    date = datetime.now().strftime("%d.%m.%Y %H:%M")
+
+    db.session.add(Tasks(description=description, is_done=is_done, date=date))
+    db.session.commit()
+
+    return redirect(url_for("views.index"))
 
 @views.route("/edit/<int:id>", methods=["GET", "POST"])
 def edit(id: int):
